9c13ca9bfb4d3120f20feb2cf7f77b71547dcc4a
shyamlal98 <shyaml.cse16@gmail.com>
2020-08-23 17:34:19 +0530

    Size of Post Articles and Carousel Fitted

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,13 +1,25 @@
 "use strict";
 
-var paraID = document.getElementById("less-content");
-var oldValue = paraID.innerHTML; // var cmtID = document.getElementById('cmt');
+var parasTags = document.getElementsByTagName("p");
+var oldValue = [];
+var newValue = []; // var cmtID = document.getElementById('cmt');
+// console.log(oldValue);
 
-console.log(paraID);
-var newVal = oldValue.substring(0, 300) + "&hellip;<button type='button' class='btn' onclick='readFullArticle();'>" + "Read More" + "<span class='glyphicon glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button> ";
-paraID.innerHTML = newVal;
-console.log(paraID.innerHTML);
+for (var i = 0; i < parasTags.length; i++) {
+  oldValue.push(parasTags[i].innerHTML);
+  newValue.push(oldValue[i].substring(0, 300) + "&hellip;<button type='button' class='btn' onclick='readFullArticle(" + i + ");'>" + "Read More" + "<span class='glyphicon glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button> ");
+  parasTags[i].innerHTML = newValue[i]; //console.log(i + "th Para \n\n" + parasTags[i].innerHTML);
+}
 
-function readFullArticle() {
-  paraID.innerHTML = oldValue;
+var prev;
+
+function readFullArticle(i) {
+  var parasTagsR = document.getElementsByTagName("p");
+
+  if (prev != undefined) {
+    parasTagsR[prev].innerHTML = newValue[prev];
+  }
+
+  parasTagsR[i].innerHTML = oldValue[i];
+  prev = i;
 }
