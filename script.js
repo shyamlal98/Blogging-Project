81d0c7ad65fa34f2a8e70166921a7fd235067a3c
shyamlal98 <shyaml.cse16@gmail.com>
2020-08-23 16:35:08 +0530

    Read More Functionality Completed Using JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-var paraID = document.getElementById("less-content");
-var oldValue = paraID.innerHTML;
-// var cmtID = document.getElementById('cmt');
-console.log(paraID);
-var newVal = oldValue.substring(0, 300) + "&hellip;<button type='button' class='btn' onclick='readFullArticle();'>" + "Read More" + "<span class='glyphicon glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button> ";
-paraID.innerHTML = newVal;
-console.log(paraID.innerHTML);
+var parasTags = document.getElementsByTagName("p");
 
-function readFullArticle() {
-    paraID.innerHTML = oldValue;
+var oldValue = [];
+var newValue = [];
+// var cmtID = document.getElementById('cmt');
+// console.log(oldValue);
+for (var i = 0; i < parasTags.length; i++) {
+    oldValue.push(parasTags[i].innerHTML);
+    newValue.push(oldValue[i].substring(0, 300) + "&hellip;<button type='button' class='btn' onclick='readFullArticle(" + i + ");'>" + "Read More" + "<span class='glyphicon glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button> ");
+    parasTags[i].innerHTML = newValue[i];
+
+    //console.log(i + "th Para \n\n" + parasTags[i].innerHTML);
+}
+var prev;
+
+
+
+function readFullArticle(i) {
+    var parasTagsR = document.getElementsByTagName("p");
+    if (prev != undefined) {
+        parasTagsR[prev].innerHTML = newValue[prev];
+    }
+    parasTagsR[i].innerHTML = oldValue[i];
+    prev = i;
 }
